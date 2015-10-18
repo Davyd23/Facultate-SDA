53287126c59e7c43e0cb83e2a693c3beb87a178e
Davyd23 <daviddedu02@gmail.com>
2015-10-18 23:38:21 +0300

    commit

--- Shell_sort.cpp
+++ Shell_sort.cpp
@@ -7,36 +7,44 @@ using namespace std;
 void shellSort(int array[]){
 
     int pas=ARRAY_SIZE/2;
+    int j,i;
 
-    while(pas!=0){  // pas descrescator
+    while(pas>0){  // pas descrescator
 
         for(int k=0;k<pas;k++){ // daca ajunge pe valoare pasului deja a trecut prin acea sectiune din vector
 
-                for(int i=pas+k;i<ARRAY_SIZE;i+=pas){
-                    if(array[i]<array[i-pas]){
-                        int aux=array[i];
-                        array[i]=array[i-pas];
-                        array[i-pas]=aux;
+                for(i=pas+k;i<ARRAY_SIZE;i+=pas){
+
+                    for(j=i-pas;j>=0;j-=pas){
+                        if(array[j]<array[i]){
+                            break;
+                        }
                     }
+
+                        int aux=array[i];
+
+                        for(int p=i;p>j;p-=pas){
+                            array[p]=array[p-pas];
+                        }
+                        array[j+pas]=aux;
                 }
+            }
+            pas/=2;
         }
-        pas--;
+
+
+
+        for(i=0;i<ARRAY_SIZE;i++){
+            cout<<array[i]<<" ";
+        }
     }
 
-    if(array[0]>array[1]){
-        int aux=array[0];
-        array[0]=array[1];
-        array[1]=aux;
-    }
 
-    for(int i=0;i<ARRAY_SIZE;i++){
-        cout<<array[i]<<" ";
-    }
-}
+
 
 int main(){
 
-int array[ARRAY_SIZE]={10,6,8,7,6,9,5,4,8,2};
+int array[ARRAY_SIZE]={10,9,8,7,6,5,4,3,2,1};
 
 shellSort(array);
 
